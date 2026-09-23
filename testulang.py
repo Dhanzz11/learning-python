bd2c7e2fbda85c5506250aeb9965b469a718482a
Dhanzz11 <Dhandanarendra09@gmail.com>
2026-09-23 19:17:18 +0800

    fix project

---
+++ testulang.py
@@ -0,0 +1,44 @@
+def check_password(password):
+    kriteria1 = len(password)==8
+    kriteria2 = any(c.isupper() for c in password)
+    kriteria3 = any(c.isdigit() for c in password)
+    jumlah_Kriteria = sum([kriteria1, kriteria2, kriteria3])
+
+    if jumlah_Kriteria == 3:
+        return "Password berkeamanan Kuat"
+    elif jumlah_Kriteria == 2:
+        return "Password berkeamanan sedang"
+    else:
+        return "Password berkeamanan lemah"
+    
+def enkripsi_pw(password, geser):
+    x =""
+    for i in password:
+        if i.isupper():
+            posisi = ord(i) - 65
+            posisi_geser = (posisi + geser) % 26
+            huruf_baru = chr(posisi_geser + 65)
+        elif i.islower():
+            posisi = ord(i) - 97
+            posisi_geser = (posisi + geser) % 26
+            huruf_baru = chr(posisi_geser + 97)
+        else:
+            huruf_baru = i
+        x = x + huruf_baru
+    return x
+
+def tulis_log(password, engkep):
+    with open("logPassword.txt", "a") as file:
+        file.write(f"password: {password} enkripsi: {engkep}\n")
+    with open("Logpassword.txt", "r") as file:
+        file.read()
+
+def main():
+    password= input("Masukan Password (usahakan berisi 8 character, kombinasi angka dan huruf besar): ")
+    geser = 3
+    sembunyikan = enkripsi_pw(password, geser)
+    hasil = check_password(password)
+    tulis_log(password, sembunyikan)
+    print(f"==> {hasil}")
+
+main()
