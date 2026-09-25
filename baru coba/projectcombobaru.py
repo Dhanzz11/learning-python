ba5705ffab37357cd166d2ec58361d2fe2614bc7
Dhanzz11 <Dhandanarendra09@gmail.com>
2026-09-26 02:05:13 +0800

    penambahan fitur ganti password

--- baru coba/projectcombobaru.py
+++ baru coba/projectcombobaru.py
@@ -1,6 +1,21 @@
 ######################################################################################
                                 # produk section
 
+def setting (password):
+    kriteria1 = len(password) == 8
+    kriteria2 = any(c.isupper() for c in password)
+    kriteria3 = any(c.isdigit() for c in password)
+    jumlah_Kriteria = sum([kriteria1, kriteria2, kriteria3])
+
+    if jumlah_Kriteria == 3:
+        return "Password sudah diganti"
+    else:
+        return "silahkan penuhi kriteria ini (password harus memiliki 8 karakter, huruf besar dan huruf kecil)"
+
+def data_password(password):
+    with open ("datapw.txt", "w") as file:
+        file.write(f"Password: {password}")
+
 def tulis_daftar(produk):
     daftar_produk = set()
     try:
@@ -70,7 +85,12 @@ def menu():
             bacadaftar_produk(produk)
             tulis_daftar(produk)
         elif pilihan == "4":
-            pass
+            print("==== SETTING ====")
+            print("A. Ganti password")
+            x = input(" ")
+            if x == "A":
+                password = input("password baru ")
+                setting(password)
         elif pilihan == "5":
             print("log Out?")
             opsi = input("y/n ")
@@ -85,19 +105,6 @@ def menu():
 
 ####################################################################################
                                 # setting section
-def data_pw(enkrip):
-    with open ("datapwmasuk.txt", "a") as file:
-        file.write (f"pw: {enkrip}")
-
-
-
-
-
-
-
-
-
-
 
 ####################################################################################
                                 # main section
